d3ba9ec098b95e09dcc7034d8a7e15557c7fbcbe
GenryMacros <ulti1235@gmail.com>
2023-05-14 23:15:07 +0300

    initial

---
+++ Source/Truly_Chess/Truly_Chess.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Truly_Chess.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Truly_Chess, "Truly_Chess" );
